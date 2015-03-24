741df8f3cfe1de39dfdd27fdf532f8a52766b5cf
Eric <190241347@qq.com>
2015-03-24 22:16:38 +0800

    combotree实例

--- MvcApplication1/COM.XXXX.WebApi/Admin/Controllers/OrganizationApiController.cs
+++ MvcApplication1/COM.XXXX.WebApi/Admin/Controllers/OrganizationApiController.cs
@@ -11,6 +11,23 @@ namespace COM.XXXX.WebApi.Admin.Controllers
 
     public class OrganizationApiController : ApiControllerBase<OrganizationRepository, Organization>
     {
+
+        public class UITree
+        {
+            public string id { get; set; }
+            public string text { get; set; }
+            public string iconCls { get; set; }
+            public string Checked { get; set; }
+            public string state { get; set; }
+            public object attributes { get; set; }
+            List<UITree> _children = new List<UITree>();
+            public List<UITree> children
+            {
+                get { return _children; }
+                set { _children = value; }
+            }
+        }
+
         public OrganizationApiController()
         {
             base.SetRepository(); 
@@ -40,6 +57,43 @@ namespace COM.XXXX.WebApi.Admin.Controllers
             }
 
             return lst;
+        }
+
+        [HttpPost]
+        public IEnumerable<UITree> GetOrganizationsComboTree(Guid? id)
+        { 
+            List<UITree> treelst=new List<UITree>();
+            List<Organization> lst = new List<Organization>();
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                lst.AddRange(Repository.Query(org => org.POrganizationID == null).ToList());
+            }
+            else
+            {
+                lst.AddRange(Repository.Query(org => org.POrganizationID == id).ToList());
+            }
+            foreach (var organization in lst)
+            {
+                  var tree = new UITree()
+                    {
+                        id = organization.ID.ToString(),
+                        text = organization.Name,
+                        iconCls = ""
+                    };
+                var orgchildren = GetOrganizationsTree(organization.ID);
+                foreach (var child in orgchildren)
+                {
+                    tree.children.Add(new UITree()
+                    {
+                        id = child.ID.ToString(),
+                        text = child.Name,
+                        iconCls = ""
+                    });
+                }
+                treelst.Add(tree);
+            }
+
+            return treelst;
         }
     }
 }
