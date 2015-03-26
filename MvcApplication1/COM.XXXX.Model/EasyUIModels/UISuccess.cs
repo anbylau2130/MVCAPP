39bd4f56b5039bd480524d701c3b0d0ef727dbe5
Eric <ailaohuyou123@qq.com>
2015-03-26 18:08:29 +0800

    xinzeng

---
+++ MvcApplication1/COM.XXXX.Model/EasyUIModels/UISuccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM.XXXX.EasyUIModels
+{
+    public class UISuccess
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ MvcApplication1/COM.XXXX.Model/EasyUIModels/UITree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM.XXXX.EasyUIModels
+{
+    public class UITree
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public string iconCls { get; set; }
+        public string Checked { get; set; }
+        public string state { get; set; }
+        public object attributes { get; set; }
+        List<UITree> _children = new List<UITree>();
+        public List<UITree> children
+        {
+            get { return _children; }
+            set { _children = value; }
+        }
+    }
+
+}
